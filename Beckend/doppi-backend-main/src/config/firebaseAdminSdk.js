3141fd133f6613ec02317f8aa3701c4464997e35
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-19 09:25:59 +0500

    Urban.Tech

--- Beckend/doppi-backend-main/src/config/firebaseAdminSdk.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = require("./doppi-951be-firebase-adminsdk-omijx-c8952842aa.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
